29a491c5eae489c8b61363dc15944605ee1c0b88
nindavirus-arch <nindavirus@gmail.com>
2026-07-14 13:54:05 +0900

    Update apiConfig.ts

--- src/lib/apiConfig.ts
+++ src/lib/apiConfig.ts
@@ -1,3 +1,3 @@
-const defaultSheetApiUrl = 'https://script.google.com/macros/s/AKfycbz0CNpoNv4LLG_8L83QciTJ34_jvLiQPWg6vCNAY5cKk96S33zx7b3ug3aJC-8bX4iHSQ/exec'
+const defaultSheetApiUrl = 'https://script.google.com/macros/s/AKfycbyyAh3E7qQAgoc_OdU_1dqD8fJ6n-41lYROKjnXXngWWl-MyOv-quIj3-fyzVm2Wpdv1g/exec'
 
 export const SHEET_API_URL = String(import.meta.env.VITE_SHEET_API_URL || defaultSheetApiUrl).trim()
